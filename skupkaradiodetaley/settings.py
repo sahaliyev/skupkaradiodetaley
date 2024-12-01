47688790d888a53f37575e9338f9bf18a3cac823
Sahil Aliyev <sahil.aliyev@ezcloudllc.com>
2024-12-01 16:20:20 +0400

    trusted origins added

--- skupkaradiodetaley/settings.py
+++ skupkaradiodetaley/settings.py
@@ -136,3 +136,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS")
