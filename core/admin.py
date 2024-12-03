fb497f87d3205b8fb04c9f87f41ddd2443844bd0
Sahil Aliyev <sahil.aliyev@ezcloudllc.com>
2024-12-03 13:15:26 +0400

    actions are added for image model

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
-from  . import  models
+from . import models
 from solo.admin import SingletonModelAdmin
-# Register your models here.
-admin.site.register(models.Images)
+
+
+@admin.action(description="Deactivate selected images")
+def deactivate_images(modeladmin, request, queryset):
+    queryset.update(active=False)
+
+
+@admin.action(description="Activate selected images")
+def activate_images(modeladmin, request, queryset):
+    queryset.update(active=True)
+
+
+@admin.register(models.Images)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'image', 'active')
+    actions = [deactivate_images, activate_images]
+
+
 admin.site.register(models.Contact, SingletonModelAdmin)
